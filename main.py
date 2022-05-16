f076266f9a4aa56791e234872cfdc907d9ee5b33
MikLz69 <mtkongsted@gmail.com>
2022-05-16 12:16:02 +0200

    finished v1

---
+++ main.py
@@ -0,0 +1,80 @@
+from typing import Tuple
+
+
+def main():
+    positions: list[list] = [[1, ''], [2, ''], [3, ''], [4, ''], [5, ''], [6, ''], [7, ''], [8, ''], [9, ''] ]
+    state = True
+    while True:
+
+        command = input("Which one would you like to cross? ")
+        command = command.lower()
+        for i in range(len(positions)):
+            if int(command) == positions[i][0]:
+                if state == True:
+                    positions[i][1] = 'x'
+                else:
+                    positions[i][1] = 'o'
+                state = not state
+            checkWinConditions(positions)
+        
+        print(positions)
+
+def checkWinConditions(positions: list[list] ):
+      for i in range(len(positions)):
+            checkHorizontalWinConditions(positions, i)
+            checkVerticalWinConditons(positions, i)
+            checkCrossWinConditions(positions, i)
+
+def checkHorizontalWinConditions(positions: list[list], i: int):
+    if i == 0 or i == 3 or i == 6:
+        if positions[i][1] == 'x':
+            if positions[i+1][1] == 'x':
+                if positions[i+2][1] == 'x':
+                    print("player x won")
+                    exit(0)
+        if positions[i][1] == 'o':
+            if positions[i+1][1] == 'o':
+                if positions[i+2][1] == 'o':
+                    print("player o won")
+                    exit(0)
+
+def checkVerticalWinConditons(positions: list[list], i: int):
+    if i == 0 or i == 1 or i == 2:
+        if positions[i][1] == 'x':
+            if positions[i+3][1] == 'x':
+                if positions[i+6][1] == 'x':
+                    print("player x won")
+                    exit(0)
+        if positions[i][1] == 'o':
+            if positions[i+3][1] == 'o':
+                if positions[i+6][1] == 'o':
+                    print("player o won")
+                    exit(0)
+
+def checkCrossWinConditions(positions: list[list], i: int):
+    if i == 0:
+        if positions[i][1] == 'x':
+            if positions[i+4][1] == 'x':
+                if positions[i+8][1] == 'x':
+                    print("player x won")
+                    exit(0)
+        if positions[i][1] == 'o':
+            if positions[i+4][1] == 'o':
+                if positions[i+8][1] == 'o':
+                    print("player o won")
+                    exit(0)
+    if i == 2:
+        if positions[i][1] == 'x':
+            if positions[i+2][1] == 'x':
+                if positions[i+4][1] == 'x':
+                    print("player x won")
+                    exit(0)
+        if positions[i][1] == 'o':
+            if positions[i+2][1] == 'o':
+                if positions[i+4][1] == 'o':
+                    print("player o won")
+                    exit(0)  
+    
+
+if __name__=="__main__":
+    main()
